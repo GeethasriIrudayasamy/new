a9b51bc3dcc9c83bbe3120e13a0197ac39073725
GeethasriIrudayasamy <geethasriirudayasamy@gmail.com>
2022-06-20 21:46:05 +0530

    Filter assignment

--- main.js
+++ main.js
@@ -55,53 +55,91 @@
 
 var form = document.getElementById("addForm");
 var itemLister = document.getElementById("items");
+var filter = document.getElementById("filter");
 
 //event
 form.addEventListener("submit", addItem);
 //delete event
 itemLister.addEventListener("click", removeItem);
+//filter event
+filter.addEventListener("keyup", filterItems);
 //add item
 function addItem(e) {
-  e.preventDefault();
+    e.preventDefault();
 
-  //input value
-  var newitem = document.getElementById("item").value;
-  //create element
-  var li = document.createElement("li");
-  //CLass is added
-  li.className = "list-group-item";
-  //Add textNode
-  li.appendChild(document.createTextNode(newitem));
+    //input value
+    var newitem = document.getElementById("item").value;
+    //create element
+    var li = document.createElement("li");
+    //CLass is added
+    li.className = "list-group-item";
+    //Add textNode
+    li.appendChild(document.createTextNode(newitem));
 
-  //create del button
-  var deleteButton = document.createElement("button");
-  //add class to del button
-  deleteButton.className = "btn btn-danger btn-sm float-right delete";
-  //textnode
-  deleteButton.appendChild(document.createTextNode("X"));
-  //appendButton
-  li.appendChild(deleteButton);
-  //append value submitted to list
-  itemLister.appendChild(li);
+    //create del button
+    var deleteButton = document.createElement("button");
+    //add class to del button
+    deleteButton.className = "btn btn-danger btn-sm float-right delete";
+    //textnode
+    deleteButton.appendChild(document.createTextNode("X"));
+    //appendButton
+    li.appendChild(deleteButton);
+    //append value submitted to list
+    itemLister.appendChild(li);
 
-  //add Edit button
-  var editButton = document.createElement("button");
-  //add class to del button
-  editButton.className = "btn btn-sm float-right edit";
-  //textnode
-  editButton.appendChild(document.createTextNode("edit"));
-  //appendButton
-  li.appendChild(editButton);
-  //append value submitted to list
-  itemLister.appendChild(li);
+    //add Edit button
+    var editButton = document.createElement("button");
+    //add class to del button
+    editButton.className = "btn btn-sm float-right edit";
+    //textnode
+    editButton.appendChild(document.createTextNode("edit"));
+    //appendButton
+    li.appendChild(editButton);
+    //append value submitted to list
+    itemLister.appendChild(li);
+
+    //add description
+    var description = document.createElement("description");
+    //add class to ddescription
+    description.className = "des";
+    //textnode
+    description.appendChild(document.createTextNode("Next element"));
+    //appenddescription
+    li.appendChild(description);
+    //append value submitted to list
+    itemLister.appendChild(li);
 }
 
 //Remove item
 function removeItem(e) {
-  if (e.target.classList.contains("delete")) {
-    if (confirm("Are you sure?")) {
-      var li = e.target.parentElement;
-      itemLister.removeChild(li);
+    if (e.target.classList.contains("delete")) {
+        if (confirm("Are you sure?")) {
+            var li = e.target.parentElement;
+            itemLister.removeChild(li);
+        }
     }
-  }
+}
+
+// Filter Items
+function filterItems(e) {
+    // convert text to lowercase
+    var text = e.target.value.toLowerCase();
+    // Get lis
+    var items = itemLister.getElementsByTagName("li");
+    var description = itemLister.getElementsByTagName("dl");
+    // Convert to an array
+    Array.from(items).forEach(function (item) {
+        var itemName = item.firstChild.textContent;
+        var desName = item.innerText;
+        if (itemName.toLowerCase().indexOf(text) != -1) {
+            item.style.display = "block";
+        } else {
+            item.style.display = "none";
+        }
+        if (desName.toLowerCase().indexOf(text) != -1) {
+            item.style.display = "block";
+        } else {
+            item.style.display = "none";
+        }
+    });
 }
